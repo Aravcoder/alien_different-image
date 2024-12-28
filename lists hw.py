4fc0facbcefa9610e3f94d73642614cad10a16c5
Aravcoder <aravanambiar@gmail.com>
2024-12-28 11:37:44 +0100

    Add files via upload

---
+++ lists hw.py
@@ -0,0 +1,4 @@
+my_list_under_30=[ 13 , 17 , 15 , 27 , 22 , 20 , 10]
+my_list_between_31_69=[ 36 , 41 , 54 , 47 , 31 , 63 , 68]
+my_list_above_70=[ 73 ,77 , 81 , 89 , 95 , 98]
+print("These are the test scores of all the students. It is aranged in different groups , The first list includes numbers >=20 , the second list includes numbers 31-69(inclusive) ,then the final list includes all socre above 70 or equal to.  " , my_list_under_30 , my_list_between_31_69 , my_list_above_70) 
